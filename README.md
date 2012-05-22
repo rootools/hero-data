9882fd65096ed40c451d70883055aa1b161ceafd
Anton Bravin <baboonweb@gmail.com>
2012-05-22 13:57:29 +0400

    Add Readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# Install
+# Usage
+```
+var hd = require('hero-data');
+
+var nowTime = hd.time.now('numeric', 'DD.MM.YYYY hh:mm:ss');
+console.log(nowTime); // Show: "22.05.2012 13:50:39"
+```
+
+## Time Module
+### .now(_type_, _format_)
--- test/main.js
+++ test/main.js
@@ -1,9 +1,13 @@
 var should = require('should');
 var heroData = require('../lib/main');
 
+
+// Time Modules here
 describe('heroData.time.now(\'numeric\', \'DD.MM.YYYY hh:mm:ss\')', function() {
-	it('aa', function() {
+	it('', function() {
 		var result = heroData.time.now('numeric', 'DD.MM.YYYY hh:mm:ss');
 		result.should.be.a('string').with.lengthOf(19);
 	});
 });
+
+// Array Modules here
